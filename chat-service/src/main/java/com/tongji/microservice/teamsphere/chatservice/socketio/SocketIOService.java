d22d845a98c2d14fc48151b3b8865d01d0383095
LLouver <2622492048@qq.com>
2023-12-22 21:21:36 +0800

    优化ws服务器,修订代码

--- chat-service/src/main/java/com/tongji/microservice/teamsphere/chatservice/socketio/SocketIOService.java
+++ chat-service/src/main/java/com/tongji/microservice/teamsphere/chatservice/socketio/SocketIOService.java
@@ -1,6 +1,8 @@
 package com.tongji.microservice.teamsphere.chatservice.socketio;
 
 import com.corundumstudio.socketio.SocketIOServer;
+import com.google.common.collect.BiMap;
+import com.google.common.collect.HashBiMap;
 import com.mongodb.client.MongoCollection;
 import com.mongodb.client.model.Filters;
 import com.mongodb.client.model.Sorts;
@@ -19,7 +21,8 @@ import java.util.*;
 
 @Service
 public class SocketIOService {
-    private static final Map<Integer, UUID> map= new HashMap<>();
+//    private static final Map<Integer, UUID> map= new HashMap<>();
+    private static final BiMap<Integer,UUID> biMap = HashBiMap.create();
     //用于连接用户数据库来获取用户信息
     @DubboReference(check = false)
     private UserService userService;
@@ -33,10 +36,10 @@ public class SocketIOService {
     @PostConstruct
     public void startServer() {
 
-        /**
-         * 用户首次连接ws服务器
-         * 1. 获取用户信息
-         * 2. 获取用户未读消息
+        /*
+          用户首次连接ws服务器
+          1. 获取用户信息
+          2. 获取用户未读消息
          */
     server.addConnectListener(client -> {
         // 在和用户握手建立连接时，就进行token校验，获取客户端的 userId
@@ -53,9 +56,11 @@ public class SocketIOService {
         if(res.getUserid()>0){
             //将int转为String
             int userid = res.getUserid();
-            if(!map.containsKey(userid)){
-                map.put(userid, client.getSessionId());
-            }
+            //该用户已经在线，则先断开上一个用户的连接
+            if(!biMap.containsKey(userid))
+                server.getClient(biMap.get(userid)).disconnect();
+            //将新的连接信息存入map
+            biMap.put(userid, client.getSessionId());
         }
         //token校验失败，直接切断链接
         else{
@@ -67,45 +72,49 @@ public class SocketIOService {
         System.out.println("Client connected: " + client.getSessionId());
     });
 
-        /**
-         * 断开连接
+        /*
+          断开连接
          */
     server.addDisconnectListener(client -> {
-        if(map.containsValue(client.getSessionId())){
-            map.remove(map.get(client.getSessionId()));
+        if(biMap.containsValue(client.getSessionId())){
+            biMap.remove(biMap.inverse().get(client.getSessionId()));
         }
         System.out.println("Client disconnected: " + client.getSessionId());
     });
+    /*
+      消息已读状态更替
+      q:id不应该由前端传递，应该由后端生成
+     */
     server.addEventListener("updateReadStatus", MessageObject.class, (client, data, ackRequest)-> {
-        int userid=data.getReceiverId();
-        if(!map.containsKey(userid)){
-            map.put(userid, client.getSessionId());
-        }
         int sender = data.getSenderId();
         int receiver = data.getReceiverId();
+
+        //更新数据库
         MongoCollection<Document> collection = MongoDB.getDatabase().getCollection("chat");
         Bson filter = Filters.and(
                 Filters.eq("sender", sender),
                 Filters.eq("receiver", receiver),
                 Filters.eq("isRead", false)
         );
-
 // Create an update operation to set isRead to true
         Bson updateOperation = Updates.set("isRead", true);
-
 // Update all matching documents in the collection
         collection.updateMany(filter, updateOperation);
-        server.getClient(map.get(userid)).sendEvent("readStatusUpdated", sender);
-//        server.getBroadcastOperations().sendEvent("readStatusUpdated", sender);
 
+        //通知对方消息已被阅读
+        server.getClient(biMap.get(sender)).sendEvent("readStatusUpdated", sender);
+//        server.getBroadcastOperations().sendEvent("readStatusUpdated", sender);
     });
+
+    /*
+      获取最近聊天对象
+     */
     server.addEventListener("requestRecentChats", String.class, (client, data, ackRequest) -> {
         System.out.println("Received userId message: " + data);
         String userId=data;
 
         MongoCollection<Document> collection = MongoDB.getDatabase().getCollection("chat");
         Set<String> contactIds = new HashSet<>();
-
                 // 查询 sender 或 receiver 为 userId 的记录
                 collection.find(
                         Filters.or(
@@ -128,13 +137,12 @@ public class SocketIOService {
                     contacts.add(new Document("contactId", contactId));
                     System.out.println(contactId);
                 }
-
                 client.sendEvent("recentChatResponse", contacts);
-
-
     });
 
-
+    /*
+    查询聊天记录
+     */
     server.addEventListener("fetchChatHistory", MessageObject.class, (client, data, ackRequest) -> {
         int sender = data.getSenderId();
         int receiver = data.getReceiverId();
@@ -160,14 +168,19 @@ public class SocketIOService {
 
     server.addEventListener("messageEvent", MessageObject.class, (client, data, ackRequest) -> {
         System.out.println("Received message: " + data);
+        //聊天记录插入数据库
+        MongoCollection<Document> collection = MongoDB.getDatabase().getCollection("chat");
+        System.out.println("Connected successfully");
+        // 将 MessageObject 转换为 Document 并插入到集合中
+        Document doc = data.toDocument();
+        collection.insertOne(doc);
+        System.out.println("Document inserted successfully");
 
-            MongoCollection<Document> collection = MongoDB.getDatabase().getCollection("chat");
-            System.out.println("Connected successfully");
-            // 将 MessageObject 转换为 Document 并插入到集合中
-            Document doc = data.toDocument();
-            collection.insertOne(doc);
-            System.out.println("Document inserted successfully");
-            server.getBroadcastOperations().sendEvent("messageEvent", data);
+//            server.getBroadcastOperations().sendEvent("messageEvent", data);
+        //将消息发给对应的人
+        if(biMap.containsKey(data.getReceiverId())){
+            server.getClient(biMap.get(data.getReceiverId())).sendEvent("messageEvent", data);
+        }
     });
 //    server.addEventListener("simpleMessageEvent", String.class, (client, data, ackRequest) -> {
 //        System.out.println("Received simple message: " + data);
