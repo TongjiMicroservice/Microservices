6cfe6d3006aaef6bece3ed365dc38778d8a8ee32
LLouver <2622492048@qq.com>
2023-12-23 14:51:54 +0800

    设计全部的Project微服务接口

--- project-service/src/main/java/com/tongji/microservice/teamsphere/projectservice/impl/ProjectServiceImpl.java
+++ project-service/src/main/java/com/tongji/microservice/teamsphere/projectservice/impl/ProjectServiceImpl.java
@@ -4,32 +4,48 @@ import com.tongji.microservice.teamsphere.dto.APIResponse;
 import com.tongji.microservice.teamsphere.dto.projectservice.MembersResponse;
 import com.tongji.microservice.teamsphere.dto.projectservice.ProjectInfoResponse;
 import com.tongji.microservice.teamsphere.dubbo.api.ProjectService;
+import com.tongji.microservice.teamsphere.entities.projectservice.ProjectData;
 import org.apache.dubbo.config.annotation.DubboService;
 
 @DubboService
 public class ProjectServiceImpl implements ProjectService {
     @Override
-    public APIResponse creatProject(){
-        return new APIResponse();
-    };
-    @Override
-    public APIResponse addProjectMember(String projectId, String memberId){
-        return new APIResponse();
-    };
-    @Override
-    public APIResponse removeProjectMember(String projectId, String memberId){
-        return new APIResponse();
-    };
-    @Override
-    public APIResponse updateProjectMemberPrivilege(String projectId, String memberId){
-        return new APIResponse();
-    };
-    @Override
-    public MembersResponse getProjectMembers(String projectId){
+    public APIResponse creatProject(String token, ProjectData projectData) {
         return null;
-    };
+    }
+
     @Override
-    public ProjectInfoResponse getProjectInfo(String projectId){
+    public APIResponse addProjectMember(String token, int projectId, int memberId) {
         return null;
-    };
+    }
+
+    @Override
+    public APIResponse updateProjectInfo(String token, int projectId, ProjectData projectData) {
+        return null;
+    }
+
+    @Override
+    public APIResponse removeProjectMember(String token, int projectId, int memberId) {
+        return null;
+    }
+
+    @Override
+    public APIResponse updateProjectMemberPrivilege(String token, int projectId, int memberId, int privilege) {
+        return null;
+    }
+
+    @Override
+    public MembersResponse getProjectMembers(String token, int projectId) {
+        return null;
+    }
+
+    @Override
+    public ProjectInfoResponse getProjectInfo(String token, int projectId) {
+        return null;
+    }
+
+    @Override
+    public APIResponse deleteProject(String token, int projectId) {
+        return null;
+    }
 }
