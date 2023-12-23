a1c8a87d0d45699f2e288516f61486265f6bee6d
luxingzhi27 <3523718637@qq.com>
2023-12-23 11:36:54 +0800

    设计好日程和会议架构

---
+++ meeting-service/src/main/java/com/tongji/microservice/teamsphere/meetingservice/mapper/MeetingMapper.java
@@ -0,0 +1,7 @@
+package com.tongji.microservice.teamsphere.meetingservice.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.tongji.microservice.teamsphere.meetingservice.entities.Meeting;
+
+public interface MeetingMapper extends BaseMapper<Meeting> {
+}
--- teamsphere-api/src/main/java/com/tongji/microservice/teamsphere/dubbo/api/MeetingService.java
+++ teamsphere-api/src/main/java/com/tongji/microservice/teamsphere/dubbo/api/MeetingService.java
@@ -2,14 +2,12 @@ package com.tongji.microservice.teamsphere.dubbo.api;
 
 import com.tongji.microservice.teamsphere.dto.APIResponse;
 import com.tongji.microservice.teamsphere.dto.meetingservice.MeetingResponse;
-import com.tongji.microservice.teamsphere.entities.meetingservice.Meeting;
 
 import java.time.LocalDateTime;
-import java.util.List;
 
 public interface MeetingService {
     APIResponse cancelMeeting(String meetingId);
     MeetingResponse getMeetingsForProject(String projectId);
     MeetingResponse getMeetingsForUser(String userId);
-    APIResponse createMeeting(String projectId, String title, String description, LocalDateTime deadline);
+    APIResponse createMeeting(String projectId, String title, String description,LocalDateTime startTime,int duration);
 }
