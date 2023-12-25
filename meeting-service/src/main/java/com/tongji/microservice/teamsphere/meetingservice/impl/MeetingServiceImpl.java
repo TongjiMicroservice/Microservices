6603c5c56982e575700c788a1ee10c250db57894
fengye7 <zcj2518529668@163.com>
2023-12-25 22:02:11 +0800

    Commit message

--- meeting-service/src/main/java/com/tongji/microservice/teamsphere/meetingservice/impl/MeetingServiceImpl.java
+++ meeting-service/src/main/java/com/tongji/microservice/teamsphere/meetingservice/impl/MeetingServiceImpl.java
@@ -3,10 +3,8 @@ package com.tongji.microservice.teamsphere.meetingservice.impl;
 import com.tongji.microservice.teamsphere.dto.APIResponse;
 import com.tongji.microservice.teamsphere.dto.meetingservice.MeetingResponse;
 import com.tongji.microservice.teamsphere.dubbo.api.MeetingService;
-import com.tongji.microservice.teamsphere.dubbo.api.UserService;
 import com.tongji.microservice.teamsphere.meetingservice.mapper.MeetingMapper;
 import com.tongji.microservice.teamsphere.meetingservice.mapper.MeetingParticipantsMapper;
-import org.apache.dubbo.config.annotation.DubboReference;
 import org.apache.dubbo.config.annotation.DubboService;
 import org.springframework.beans.factory.annotation.Autowired;
 
