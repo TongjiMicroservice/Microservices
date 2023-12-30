ebdf9ed6eed67b93c306e7030893222f096be716
LLouver <2622492048@qq.com>
2023-12-30 10:44:31 +0800

    修改yml文件

--- file-service/src/main/java/com/tongji/microservice/teamsphere/fileservice/TeamsphereFileApplication.java
+++ file-service/src/main/java/com/tongji/microservice/teamsphere/fileservice/TeamsphereFileApplication.java
@@ -11,6 +11,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class TeamsphereFileApplication {
 
     public static void main(String[] args) {
+        
         SpringApplication.run(TeamsphereFileApplication.class, args);
     }
 
